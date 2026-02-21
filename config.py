79115daef291dd7b03d850b1aeec7bd49ecf3092
SihagBots <sihagamit308@gmail.com>
2026-02-21 07:28:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ import os
 # ==============================
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", "0"))
+API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 
 
